dd837d87a1012c424d7addd02342c6baaf5c2321
Jesse Talavera-Greenberg <jesse@jesse.tg>
2021-11-12 19:47:29 -0500

    Add SpanExtensions

---
+++ Assets/YMFM/Vgm/SpanExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Buffers.Binary;
+
+namespace Ymfm.Vgm
+{
+    public static class SpanExtensions
+    {
+        public static uint ReadUInt32LittleEndian(this ReadOnlySpan<byte> span, uint offset)
+        {
+            return BinaryPrimitives.ReadUInt32LittleEndian(span[(int)offset..(int)(offset + sizeof(int))]);
+        }
+
+        public static ushort ReadUInt16LittleEndian(this ReadOnlySpan<byte> span, uint offset)
+        {
+            return BinaryPrimitives.ReadUInt16LittleEndian(span[(int)offset..(int)(offset + sizeof(ushort))]);
+        }
+    }
+}
