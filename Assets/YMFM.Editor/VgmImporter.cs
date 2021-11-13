ec1acbced0930a99edc20b50f745546cb23fda2b
Jesse Talavera-Greenberg <jesse@jesse.tg>
2021-11-12 19:51:19 -0500

    Clean up VgmImporter

--- Assets/YMFM.Editor/VgmImporter.cs
+++ Assets/YMFM.Editor/VgmImporter.cs
@@ -6,15 +6,14 @@ using Ymfm.Vgm;
 
 namespace Ymfm.Editor
 {
-    [InitializeOnLoad]
-    [ScriptedImporter(1, new[] { "vgm", "vgz" })]
+    [ScriptedImporter(2, new[] { "vgm", "vgz" })]
     internal sealed class VgmImporter : ScriptedImporter
     {
         public override void OnImportAsset([NotNull] AssetImportContext ctx)
         {
             var vgm = File.ReadAllBytes(ctx.assetPath);
 
-            var data = VgmFile.CreateInstance(vgm); 
+            var data = VgmFile.CreateInstance(vgm);
 
             ctx.AddObjectToAsset("VGM", data);
             ctx.SetMainObject(data);
