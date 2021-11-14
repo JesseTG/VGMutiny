dea4a92f61d3b391db989be61d0c02f3594067ed
Jesse Talavera-Greenberg <jesse@jesse.tg>
2021-11-14 15:04:39 -0500

    Unmark an abstract method as unsafe

--- Assets/YMFM/Vgm/BaseVgmChip.cs
+++ Assets/YMFM/Vgm/BaseVgmChip.cs
@@ -18,7 +18,7 @@ namespace Ymfm.Vgm
         }
 
         public abstract void Write(uint reg, byte data);
-        public abstract unsafe void Generate(long outputStart, long outputStep, Span<int> buffer);
+        public abstract void Generate(long outputStart, long outputStep, Span<int> buffer);
         public abstract void WriteData(AccessClass type, uint @base, ReadOnlySpan<byte> src);
         public abstract void SeekPcm(uint pos);
         public abstract byte ReadPcm();
